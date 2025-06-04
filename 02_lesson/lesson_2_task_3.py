f184841e4ecfb4c8991143d70b8327ad69b61f83
valentinaplus <valentinaplus20gmail.com>
2025-06-04 12:53:35 +0300

    02_lesson

---
+++ 02_lesson/lesson_2_task_3.py
@@ -0,0 +1,10 @@
+def square(side):
+    area = side ** 2
+    return area
+
+
+side_length = 5
+
+square_area = square(side_length)
+
+print(f"Площадь квадрата со стороной {side_length}): {square_area}")
---
+++ 02_lesson/lesson_2_task_7.py
@@ -0,0 +1,3 @@
+my_list = list(range(18, 14, 10, 6, 2))
+
+print(my_list)
