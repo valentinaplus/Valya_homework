e6e3d32838ee2e51f33e8bf1f219f42920a1cdd2
valentinaplus <valentinaplus20gmail.com>
2025-07-25 17:13:45 +0300

    Добавлена папка 08_lesson с файлами для lesson8

---
+++ 08_lesson/test_projects.py
@@ -0,0 +1,62 @@
+import pytest
+from api_client import YougileAPI
+
+base_url = "https://ru.yougile.com/"
+token = "DNrUX2O5iTVglkKs444HyLIghBHDDvKiIFK5hAOou-U-fziljYiZBz0bBmTbP0b3"
+
+
+@pytest.fixture(scope="module")
+def api():
+    return YougileAPI(base_url, token)
+
+
+@pytest.fixture()
+def created_project(api):
+    response = api.create_project("Проекты для теста")
+    assert response.status_code == 201
+    return response.json()
+
+
+def test_post_project_positive(api):
+    response = api.create_project("Создать")
+    print(response)
+    assert response.status_code == 201
+    data = response.json()
+    assert "id" in data
+
+
+def test_post_project_negative_missing_title(api):
+    response = api.create_project("")
+    assert response.status_code in (400, 422)
+
+
+def test_put_project_positive(api, created_project):
+    project_id = created_project["id"]
+    new_users = {"8a0b723e-0f0d-4348-b717-31bfec8380b2": "admin"}
+    response = api.update_project(project_id, title="Обновленный проект",
+                                  users=new_users)
+    assert response.status_code == 200
+    data = response.json()
+    assert data["id"] == project_id
+
+
+def test_put_project_negative_invalid_id(api):
+    project_id = 123
+    new_users = {"8a0b723e-0f0d-4348-b717-31bfec8380b2": "admin"}
+    response = api.update_project(project_id, title="Обновленный проект",
+                                  users=new_users)
+    assert response.status_code == 404
+
+
+def test_get_project_positive(api, created_project):
+    project_id = created_project["id"]
+    response = api.get_project(project_id)
+    assert response.status_code == 200
+    data = response.json()
+    assert data["id"] == project_id
+
+
+def test_get_project_negative_invalid_id(api):
+    invalid_id = "00000000-0000-0000-0000-000000000000"
+    response = api.get_project(invalid_id)
+    assert response.status_code == 404
